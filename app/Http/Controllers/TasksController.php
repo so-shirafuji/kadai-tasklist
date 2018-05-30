cd97a1177221990d2eee56fd73283d471b2a8213
EC2 Default User <ec2-user@ip-172-31-1-165.us-east-2.compute.internal>
2018-05-30 07:25:19 +0000

    saisaiteishutu

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -42,10 +42,12 @@ class TasksController extends Controller
      public function store(Request $request)
     {
         $this->validate($request,[
-            "content"=>"required|max::10",
+            "status"=>"required|max:10",
+            "content"=>"required|max:191",
             ]);
         
         $task = new Task;
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
@@ -91,7 +93,13 @@ class TasksController extends Controller
      */
      public function update(Request $request, $id)
     {
+        $this->validate($request,[
+            "status" =>"required|max:10",
+            "content" =>"required|max:191"
+        ]);
+        
         $task = Task::find($id);
+        $task->status = $request->status;
         $task->content = $request->content;
         $task->save();
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,15 +2,16 @@
 
 @section('content')
 
-    <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+<h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
 
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
 
         {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
 
 @endsection
