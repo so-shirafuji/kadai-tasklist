4728dfdb1bdbcee287e45757d760e61e91bf97d0
EC2 Default User <ec2-user@ip-172-31-1-165.us-east-2.compute.internal>
2018-05-31 01:43:01 +0000

    kadaiteoishutu

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,17 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+    <h1>id:{{$task->id}}のメッセージ編集ページ</h1>
+    
+     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
-<h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
-
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
+         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
-
+        
         {!! Form::label('content', 'メッセージ:') !!}
         {!! Form::text('content') !!}
 
         {!! Form::submit('更新') !!}
+
+    {!! Form::close() !!}
 
 @endsection
